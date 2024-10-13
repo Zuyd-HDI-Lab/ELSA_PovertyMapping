5e906a3bf38847af338283cdc6629623074c1e35
Mareno Demmers <mhjdemmers@gmail.com>
2024-10-13 16:49:50 +0200

    implement tooltips

--- map-projection-d3/src/App.js
+++ map-projection-d3/src/App.js
@@ -5,15 +5,20 @@ import Map from './Map';
 function App() {
   const [geojson, setGeojson] = useState(null);
 
+  // Fetch GeoJSON data
   useEffect(() => {
-    const localPath = '/heerlen_buurten_2023.json'
+    const localPath = '/heerlen_buurten_2023_formatted.json'
     const url = 'https://gist.githubusercontent.com/mhjdemmers/2da23018a024c29c01690502a7f65210/raw/gistfile1.txt';
     fetch(localPath)
       .then(response => response.json())
-      .then(data => setGeojson(data))
+      .then(data => {
+        console.log('Fetched GeoJSON Data:', data); // debug line
+        setGeojson(data);
+      })
       .catch(error => console.error('Error fetching the GeoJSON data:', error));
   }, []);
 
+  // Render the Map component if geojson data is available, otherwise show a loading message
   return (
     <div className="App">
       {geojson ? <Map geojson={geojson} /> : <p>Loading map...</p>}
--- map-projection-d3/src/Map.js
+++ map-projection-d3/src/Map.js
@@ -1,15 +1,38 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 
 const Map = ({ geojson }) => {
   const svgRef = useRef();
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [tooltipContent, setTooltipContent] = useState('');
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseOver = (event, d) => {
+    console.log('Mouse Over:', d); //debug line
+    const name = d.properties.buurtnaam || `Unnamed Feature, id: ${d.properties.id}`;
+    setTooltipContent(name);
+    setTooltipPosition({ x: event.pageX, y: event.pageY });
+    setShowTooltip(true);
+  };
+
+  const handleMouseMove = (event) => {
+    // console.log('Mouse Move:', event.pageX, event.pageY); //debug line
+    setTooltipPosition({ x: event.pageX, y: event.pageY });
+  };
+
+  const handleMouseOut = () => {
+    // console.log('Mouse Out'); //debug line 
+    setShowTooltip(false);
+  };
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
     const width = 800;
     const height = 600;
 
+    // Create a Mercator projection and fit it to the GeoJSON data https://d3js.org/d3-geo/projection
     const projection = d3.geoMercator().fitSize([width, height], geojson);
+    // Convert GeoJSON into SVG path 
     const path = d3.geoPath().projection(projection);
 
     svg
@@ -21,10 +44,33 @@ const Map = ({ geojson }) => {
       .append('path')
       .attr('d', path)
       .attr('fill', 'lightblue')
-      .attr('stroke', 'black');
+      .attr('stroke', 'black')
+      .on('mouseover', handleMouseOver)
+      .on('mousemove', handleMouseMove)
+      .on('mouseout', handleMouseOut);
   }, [geojson]);
 
-  return <svg ref={svgRef}></svg>;
+  return (
+    <div>
+      <svg ref={svgRef}></svg>
+      {showTooltip && (
+        <div
+          style={{
+            position: 'absolute',
+            top: `${tooltipPosition.y}px`,
+            left: `${tooltipPosition.x}px`,
+            backgroundColor: 'white',
+            padding: '5px',
+            border: '1px solid black',
+            borderRadius: '3px',
+            pointerEvents: 'none'
+          }}
+        >
+          {tooltipContent}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Map;
