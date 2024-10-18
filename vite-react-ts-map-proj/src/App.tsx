3a00754ed149fce766bb9801c4e340c155e73e9c
Mareno Demmers <mhjdemmers@gmail.com>
2024-10-18 10:33:52 +0200

    Revert "rewrite in typescript"
    This reverts commit b9b6a2a94eba29defc332bd6f9cbaab35abf736a.

--- vite-react-ts-map-proj/src/App.tsx
+++ vite-react-ts-map-proj/src/App.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
-import './App.css';
-import Map from './Map';
-import useDataLoader from './DataLoader';
-import useMapDataLoader from './MapDataLoader';
-import DataVisualizer from './DataVisualizer';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App: React.FC = () => {
-  const dataLocalPath = '/data.csv';
-  // const dataUrl = ''
-  const mapLocalPath = '/heerlen_buurten_2023_formatted.json';
-  // const mapUrl = 'https://gist.githubusercontent.com/mhjdemmers/2da23018a024c29c01690502a7f65210/raw/gistfile1.txt';
-  const mapData = useMapDataLoader(mapLocalPath);
-  const data = useDataLoader(dataLocalPath);
+function App() {
+  const [count, setCount] = useState(0)
 
-  console.log('Map Data:', mapData); // debug line
-  console.log('Vis Data:', data); // debug line
-
-  // Render the Map component if geojson data is available, otherwise show a loading message
   return (
-    <div>
-      {mapData && <Map mapData={mapData} />}
-      {data.length > 0 && <DataVisualizer data={data} mapData={mapData} />}
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
