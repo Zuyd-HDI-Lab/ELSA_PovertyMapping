77b8916857c31d78666fd20035bd2c6af60520c5
Mareno Demmers <mhjdemmers@gmail.com>
2024-11-14 14:44:02 +0100

    fix: remove unused filter options from constants

--- poverty-mapping/src/components/Viz/constants.ts
+++ poverty-mapping/src/components/Viz/constants.ts
@@ -2,8 +2,6 @@ import { LegendContent } from '../Legend';
 
 export const Filterlist = [
     { value: ">10", label: "Above 10%" },
-    { value: "filter2", label: "Filter 2" },
-    { value: "filter3", label: "Filter 3" }
 ];
 
 export const VizLegend: LegendContent = {
