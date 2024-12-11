8fe3100a6c88202fd40786622d61ecad2b0b9b6b
Mareno Demmers <mhjdemmers@gmail.com>
2024-12-11 09:55:20 +0100

    feat: Filter input data in transformData utility to include only relevant series

--- poverty-mapping/src/utils/transformData.ts
+++ poverty-mapping/src/utils/transformData.ts
@@ -13,32 +13,36 @@ export interface LineChartData {
 }
 
 export const transformData = (data: InputData[]): LineChartData[] => {
-    const ret = data.map((item) => {
+    const filteredData = data.filter(item => item.WijkenEnBuurten.startsWith('BU'));
+    
+    const ret = filteredData.map((item) => {
         const year = item.Perioden.slice(0, 4);
         const month = item.Perioden.slice(6, 8);
-
+        
         const isoDate = `${year}-${month}-01`;
-
+        
         return {
             date: isoDate,
             value: item.Bijstandsuitkering_10,
             series: item.WijkenEnBuurten
         }
     });
-
+    
     console.log(data)
+    console.log(filteredData)
     console.log(ret)
 
-    return  data.map((item) => {
-        const year = item.Perioden.slice(0, 4); 
-        const month = item.Perioden.slice(6, 8); 
+    return ret
+    // return  data.map((item) => {
+    //     const year = item.Perioden.slice(0, 4); 
+    //     const month = item.Perioden.slice(6, 8); 
 
-        const isoDate = `${year}-${month}-01`;
+    //     const isoDate = `${year}-${month}-01`;
 
-        return {
-            date: isoDate,
-            value: item.Bijstandsuitkering_10,
-            series: item.WijkenEnBuurten
-        }
-    });
+    //     return {
+    //         date: isoDate,
+    //         value: item.Bijstandsuitkering_10,
+    //         series: item.WijkenEnBuurten
+    //     }
+    // });
 };
